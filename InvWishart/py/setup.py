0163c4ab93ec38dc33f00ebf4202df39b86e91fd
kousu <nick@kousu.ca>
2014-03-17 19:35:12 -0400

    Building works; module isn't placed in a nice place, though

--- InvWishart/py/setup.py
+++ InvWishart/py/setup.py
@@ -2,6 +2,9 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
+from Cython.Distutils import build_ext as build_cython
+
 setup(name="primes_demo",
+     cmdclass = {'build_ext': build_cython},
      ext_modules = [Extension("primes", ["primes.pyx"])]
      )
