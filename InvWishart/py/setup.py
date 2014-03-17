436ec5c0e7f7515e41514c19e2c2086ad63dc815
kousu <nick@kousu.ca>
2014-03-17 19:50:41 -0400

    Make the output come out somewhere obvious.

--- InvWishart/py/setup.py
+++ InvWishart/py/setup.py
@@ -4,10 +4,13 @@ from distutils.extension import Extension
 
 from Cython.Distutils import build_ext as build_cython
 
-# include_dirs = [numpy.get_include()]) <-- will need this
+build_cython.inplace = True  #make the output come out somewhere obvious
+                             #XXX this is incompatible with 'clean's default behaviour; I get 'primes.c' left uncleaned
+
+# include_dirs = [numpy.get_include()]) <-- will need this eventually
 
 setup(name="primes_demo",
-     packages = [''],
+     packages = [''], # <-- will need this eventually
      cmdclass = {'build_ext': build_cython},
      ext_modules = [Extension("primes", ["primes.pyx"])]
      )
