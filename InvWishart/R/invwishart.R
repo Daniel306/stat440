2b83323c3d218ebb63b9aeab177900cd851ed7bd
kousu <nick@kousu.ca>
2014-03-17 23:20:39 -0400

    Comments and the EXTREMELY naive invwishart algorithm

--- InvWishart/R/invwishart.R
+++ InvWishart/R/invwishart.R
@@ -1,18 +1,75 @@
 
+
+# Style plan:
+#  -follow Google's R style guide
+
+# API plan:
+#  -do we conform to the API of rWishart ("(df, V)") or use the dist'n parameters ("(V, df)")?
+
+
+# Testing plan:
+#  -testing is ...hard?
+#  - well, for one, we can generate rWisharts, then generate their Inverses
+#  - use integrate() to check summing to 1 (tho as d increases you will)
+# correctness testing:
+#  - we need to compare distributions; this is hard, because our distributions are on matrices (or generally, vectors)
+#  - we can snitch HW1's code to make plots showing marginal histograms against kernel density estimates for
+#     a vectorized (might need some retrofitting to generalize it to all vectors? or maybe we just insist its for matrices)
+#  - 
+#
+# performance testing
+#  - compare the naive R implementation with an Rcpp implementation
+#  - compare the naive SciPy implementation with an
+#  - compare the less naive implementation (ie the one given us by Lysy) in: pure R, Rcpp, Scipy, Pyrex
+#  -
+#  comparison algorithm is:
+#   - parameter sweep over (n, d, df, implementation) -- use a random V in each case, and record runtimes
+#   - plot runtimes as functions of each dimension
+#   - plot runtimes in 5d space via magic (actually, break it down first splitting by implementation, then
+#   - include confidence intervals (bands??)
+
+
 # notation: ~ 'distributed as'
 #           W 'Wishart distribution'
 #       inv(W)'inverse Wishart distribution' -- this is the distribution of matrices whose inverses are Wishart distributed
 #           V 'a variance matrix'
 #           n 'an int, the 'degrees of freedom' in question" (see [Chi-Squared Distribution]] and [[Wishart Distribution]] for defitinion of the last two)
-# Wikipedia says
-# X ~ W(V,n)  <=>   inv(X) ~ inv(W)(inv(V), n)
-# so, a trivial (slow, but correct) algorithm for (approximating) inverse wisharts is:
+#
+
+#######################################3
+### IMPORTS
+###
 
 require("matrixcalc");
 
-multigamma <- function(x, p) {
-  
+#######################################
+### UTILS
+
+multigamma <- function(x, d) {
+  # usage: multigamma(x, d)
+  #
+  # x: the point to evaluate at
+  # d: the dimensionality of the multigamma
+  # 
+  prod(x + (1-1:d)/2) / sqrt(sqrt(pi^(d*(d-1))))
 }
+
+
+multigamma <- function(d) {
+  # usage: multigamma(d)(x)
+  
+  function(x) {
+  #
+  # x: the point to evaluate at
+  # d: the dimensionality of the multigamma
+  # 
+  prod(x + (1-1:d)/2) / sqrt(sqrt(pi^(d*(d-1))))
+  }
+}
+
+
+#######################################
+### PDFS
 
 pWishart <- function(X, V, df) {
   #TODO: docs
@@ -65,15 +122,41 @@ pInvWishart <- function(X, V, df) {
   pWishart(solve(X), solve(V), df);
 }
 
+
+#######################################
+### CDFS
+
 # multivariate cdfs don't make much sense, because you would
 # need to specify what direction you're integrating in
 # and more critically because they need to take a ~boundary~ to integrate up to, they can't just take a single ~point~.
 # so we don't provide one
 # you can use integrate() to approximate a region of interest.
 
-rInvWishart <- function(n, df, Sigma) {
+
+#######################################
+### SIMULATORS
+
+rInvWishart.naive <- function(n, df, Sigma) {
+  # naive algorithm:
+  # Wikipedia says
+  # X ~ W(V,n)  <=>   inv(X) ~ inv(W)(inv(V), n)
+  # so, a trivial and correct (but slow?? TODO: TEST) algorithm for (approximating) inverse wisharts is:
+  
   rWishart(n, df, solve(Sigma));
 }
 
-# tests:
 
+rInvWishart.extremelynaive <- function(n, df, Sigma) {
+  # extremely naive algorithm:
+  # the Inverse Wishart distribution is defined as the distribution of matrices who are the inverse of matrices that are Wishart distributed
+  
+  R = rWishart(n, df, Sigma);
+  # now, invert all of them
+  for(i in 1:n) {
+     R[,,i] = solve(R[,,i])
+  }
+  R
+}
+
+
+rInvWishart = rInvWishart.naive
