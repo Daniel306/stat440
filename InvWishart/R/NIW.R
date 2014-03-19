dec5bbca5f0db3a04bf45ff62e9470ee9d3f365a
kousu <nick@kousu.ca>
2014-03-19 00:00:39 -0400

    experimenting with the efficient algorithm; there's seems to be an inconsistency here:
    U_i does NOT give a lower triangular matrix, which means that we cannot use back-solving.

--- InvWishart/R/NIW.R
+++ InvWishart/R/NIW.R
@@ -21,10 +21,62 @@ rNIW.naivesample <- function(Mu, kappa, Psi, df) {
 }
 
 
-rNIW.snappysample <- function(Mu, kappa, Psi, df) {
+BartlettFactor <- function(d, df) {
+  # the Bartlett decomposition of X ~ W(V, df) is
+  #   X = gamma*A*A'*gamma'
+  # where  L = chol(V) (lower-triangular chol) and
+  #        A is defined as A[i,i] ~ sqrt(X^2_{df-{i-1}}) && A[i,j] ~ N(0,1) and A[j,i] = 0 (i>j)
+  
+  # the base case (or a special case, depending on how you look at it) is
+  #  W = AA' which has W ~ W(I, df)  <-- this is the Bartlett Theorem
+  #  it's not hard to derive from linearity of Normals and the def'n of the Wishart Dist that
+  # CWC' ~ W(CIC', df), so you can get any Wishart you want from W(I, df)
+  # (tho maybe it is hard to see that with non-integer df. :shrug:)
+  
+  A = matrix(nrow=d, ncol=d)
+  A[,] = 0
+  df = df-(1:d)+1 #rchiqsq will vectorize over its DEGREES OF FREEDOM which is super clever
+  diag(A) = sqrt(rchisq(d, df))
+    
+  # set the below-diagonals to N(0,1)s
+  i_lower = col(A) < row(A)
+  A[i_lower] = rnorm(sum(i_lower)) #sum() on a logical finds the len() of the lower triangle
+  
+  A
+}
 
-# attempt to write out the efficient algorithm (but in R, of course)
+rNIW.snappysample <- function(Mu, kappa, Psi, df) {
+  d = length(Mu) #just shorthand, not for typechecking
+  # attempt to write out the efficient algorithm (but in R, of course)
+  
+  
+  gamma = t(chol(Psi)) #t() because in R chol gives the upper triangle (the right term) and I wrote my derivations in terms of the lower
+  
+  # 
+  
      # construct L_i
+  L_i = BartlettFactor(d, df)
+  message("Lower triangular bartlett factor")
+  print(L_i)
+  
+  U_i = gamma%*%t(solve(L_i)) #hmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+  print(U_i)
+  
+  # 
+  # now, Sb%*%t(Sb) ~ W(I, n) according to Bartlett's Method according to the assignment
+  # but we want W(V, n-1)
+  # we can use part i), which says if S ~ W(I, n) then LSL' ~ W(LIL', n)  (NB, in case i forget: ' means "transpose")
+  # We want LIL' = LL' to be V, so we want to apply the choklesky decomposition to get square roots:
+  L = chol(V) # note: R's chol() gives an *upper* triangle... 
+    # and L'L = V; tho since the inner term of W(I, n) is I, it doesn't matter if we decompose the wrong way..
+    # since S ~ W(I, n) => L'SL ~ W(L'IL, n) ~ W(V, n)
+  # Sb%*%t(Sb) ~ W(I, n), so (L%*%Sb)%*%t(L%*%Sb) = L(SbSb')L' ~ W(LL', n)
+  # so we can build Sb subsequently by taking the lower triangular matrix, scaling, and squaring:
+  Sb = t(L)%*%Sb  #lower-triangular * lower triangular = lower-triangular
+  Sb = Sb%*%t(Sb)
+  Sb
+
+  
      # construct W_i (?)
      # construct U_i (?)
      # construct V_i
@@ -37,6 +89,7 @@ list(X=X, V=V);
 # 
 
 rNIW.sample = rNIW.naivesample
+rNIW.sample = rNIW.snappysample
 
 rNIW.wrapper <- function(n, Mu, kappa, Psi, df) {
   # n: the desired number of sample points (X, V) ~ NIW(Mu, kappa, Psi, df)
@@ -77,9 +130,10 @@ rNIW.wrapper <- function(n, Mu, kappa, Psi, df) {
 
 rNIW = rNIW.wrapper
 
-kMu = c(0.25, -1.5, 0.33)
+kMu = c(0.25, -1.5, 0.33, 9)
 kKappa = 1
-kV  = cbind(c(2.84, 0.43, 0.16), c(0.43, 1.52, -0.24), c(.16, -.24, 4.49))
+kV  = cbind(c(2.84, 0.43, 0.16, .5), c(0.43, 1.52, -0.24, -.73), c(.16, -.24, 4.49, 88), c(1,-2,3,4))
+kV  = t(kV)%*%kV
 kDF = 3.3
 
 
