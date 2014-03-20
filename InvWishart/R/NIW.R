fb935cbf00c5b0b542f0f287fbb46a22adf35f99
kousu <nick@kousu.ca>
2014-03-20 02:34:43 -0400

    Wrote test() method which makes nice plots automatically.
    To do this, rewrote much of the instrumentation in CPS
     (Continuation-Passing-Style) and exploited R's lispy roots.

--- InvWishart/R/NIW.R
+++ InvWishart/R/NIW.R
@@ -2,6 +2,8 @@
 require(MASS); #for rmvnorm
 rmvnorm <- mvrnorm; mvrnorm <- NULL; #repair naming convention
 
+##################################
+### Naive Sampling
 
 rInvWishart <- function(n, df, Sigma) {
  # inverse wishart sampler 
@@ -14,14 +16,35 @@ rInvWishart <- function(n, df, Sigma) {
  W
 }
 
-rNIW.naivesample <- function(Mu, kappa, Psi, df) {
-  # naive slow-as-mud Normal-Inverse-Wishart sampler
-  V = rInvWishart(1, df, Psi)[,,1]; #sample only one 
-  X = rmvnorm(1, Mu, V/kappa);
-  
-  list(X=X, V=V);
+
+rNIW.extremelynaive <- function(n, d, Mu, kappa, Psi, df, V, X) {
+  # generate the n samples by n times doing: i) generate V, ii) generate X given V.
+
+  for(i in 1:n) {
+     V[,,i] = rInvWishart(1, df, Psi)[,,1]; #sample only one 
+     X[,i] = rmvnorm(1, Mu, V[,,i]/kappa);  #ditto
+  }
+  list(V=V, X=X)
 }
 
+
+rNIW.naive <- function(n, d, Mu, kappa, Psi, df, V, X) {
+  # generate the n samples
+  # slightly less totally durpy implementation
+  V = rInvWishart(n, df, Psi);   #ignore the prealloc'd space
+  
+  # must be in a loop because each sample has a different distribution
+  for(i in 1:n) {
+     X[,i] = rmvnorm(1, Mu, V[,,i]/kappa);
+  }
+  list(V=V, X=X)
+}
+
+
+
+
+#######################################################
+### SnappySampling by exploiting Bartlett Decomposition
 
 BartlettFactor <- function(d, df) {
   # the Bartlett decomposition of X ~ W(V, df) is
@@ -49,8 +72,12 @@ BartlettFactor <- function(d, df) {
 
 #require("Matrix") #for faster(?) solving routines
 
-rNIW.snappysample <- function(Mu, kappa, Psi, df) {
-  d = length(Mu) #just shorthand, not for typechecking
+# the diff with snappy2 is that it does a backsolve() and a ..forwardtriangularmultiply(???) to compute U.inv%*%z
+# otherwise it's identical
+# and the time is STILL dominated by the need to invert
+# and also snappy2 precomputes chol(Psi), because there's no need to be redoing that..
+
+rNIW.snappy1.sample <- function(d, Mu, kappa, Psi, df) {
   # attempt to write out the efficient algorithm (but in R, of course)
   
   
@@ -66,7 +93,8 @@ rNIW.snappysample <- function(Mu, kappa, Psi, df) {
   # okay, well, it seems we're stuck with inverting A
   U = gamma
   # lawl wat: identity matrix in R is "diag(scalar)". wowwww.
-  U.inv = gamma.inv %*% backsolve(A, diag(d))
+  A.inv = backsolve(A, diag(d))
+  U.inv = gamma.inv %*% A.inv  #(U = AG and U'U = G'A'AG = W the Wishart-distributed matrix we never actually compute)
   
   V = U.inv %*% t(U.inv) #note well that this is not a LU decomposition -- it's UL
   # what. wait.. is this a PRECISION matrix or a COVARIANCE matrix?
@@ -97,12 +125,28 @@ list(X=X, V=V);
 
 # 
 
-rNIW.sample = rNIW.naivesample
 
 
-rNIW.wrapper <- function(n, Mu, kappa, Psi, df) {
+rNIW.snappy1 <- function(n, d, Mu, kappa, Psi, df, V, X) {
+  # generate the n samples by n times doing: i) generate V, ii) generate X given V.
+  for(i in 1:n) {
+     sample = rNIW.snappy1.sample(d, Mu, kappa, Psi, df)
+     V[,,i] = sample$V
+     X[,i] = sample$X
+  }
+  list(V=V, X=X)
+}
+
+
+
+
+#########################3
+## Hosting code
+
+rNIW.typechecks <- function(rNIW.sampleloop, n, Mu, kappa, Psi, df) {
   # n: the desired number of sample points (X, V) ~ NIW(Mu, kappa, Psi, df)
   # df need not be integer; instead it can be anything in the domain of the gamma function
+  #  rNIW.sampleloop: a continuation (as in continuation-passing-style) to go to when the typechecks are done
   
   # this subroutine is just the safe(ish) type-checked outer call
   # which calls rNIW.sample to get the real samples (so you can experiment with swapping out different samplers)
@@ -113,31 +157,23 @@ rNIW.wrapper <- function(n, Mu, kappa, Psi, df) {
   d = dim(Psi)[1]
   stopifnot(d == dim(Psi)[0]) #Psi must be square
   stopifnot(d == length(as.vector(Mu)))  #Mu must be the same dimensionality as the covariance matrix it supposedly goes with
+
   
   # pre-allocate space
   X = rep(0, d*n);
   dim(X) = c(d, n);
-  
+
   V = rep(0, d^2*n);
   dim(V) = c(d,d,n);
-  
-  # generate the n samples
-  for(i in 1:n) { 
-     #message(paste("sample #", i, sep=""))
-     sample = rNIW.sample(Mu, kappa, Psi, df);
-     X[,i] = sample$X
-     V[,,i] = sample$V
-     #message("X:")
-     #print(sample$X)
-     #message("V:")
-     #print(sample$V)
-     #message()
-  }
-  list(V=V, X=X)
+
+    
+  rNIW.sampleloop(n, d, Mu, kappa, Psi, df, V, X)
 }
 
 
-rNIW = rNIW.wrapper
+################################3
+### TESTING
+
 
 kMu = c(0.25, -1.5, 0.33, 9)
 kKappa = 1
@@ -145,36 +181,50 @@ kV  = cbind(c(2.84, 0.43, 0.16, .5), c(0.43, 1.52, -0.24, -.73), c(.16, -.24, 4.
 kV  = t(kV)%*%kV #after I added digits at random, force kV to be some positive def matrix
 kDF = 7.32 # must be at least as large as the dimension of kV
 
-N = 324233
 
+test <- function(method, N=32) {
+  name = deparse(substitute(method)) #LOL R
+
+
+message(paste("Beginning",name))
 tic = proc.time()
-Baseline.Samples = rNIW(N, kMu, kKappa, kV, kDF)
+Samples = rNIW.typechecks(method, N, kMu, kKappa, kV, kDF)
 toc = proc.time()
-runtime1 = toc - tic
+runtime = toc - tic
+
+message(paste("Runtime for", name))
+print(runtime)
 
 
-# switch to using snappysample and redo
-rNIW.sample = rNIW.snappysample
-tic = proc.time()
-Snappy.Samples = rNIW(N, kMu, kKappa, kV, kDF)
-toc = proc.time()
-runtime2 = toc - tic
+# plot it. plot it alllll.
 
+par(mfrow=c(1,1))
+plot(c(), c(), xlim=c(0,0), ylim=c(0,0), main=name, sub="you love it")
 
 par(mfrow=c(2,2))
-# plot it. plot it alllll.
-for(i in 1:4) { for(j in 1:4) {
-  hist(Snappy.Samples$V[i,j,], main=paste("Hist of ",i,",",j),  probability=T, breaks=20, col='red')
-  lines(density(Snappy.Samples$V[i,j,]), col='steelblue')
-  lines(density(Baseline.Samples$V[i,j,]), col='black')
+for(i in 1:4) { for(j in 1:4) { #XXX hardcoded
+  hist(Samples$V[i,j,], main=paste("Hist of V[",i,",",j,"]"),  probability=T, breaks=20, col='red')
+  if(exists("Baseline.Samples")) {
+    lines(density(Baseline.Samples$V[i,j,]), col='black')
+  }
 }}
 
 for(i in 1:4) {
-  hist(Snappy.Samples$X[i,], probability=T, breaks=20, col='red')
-  lines(density(Snappy.Samples$X[i,]), col='steelblue')
-  lines(density(Baseline.Samples$X[i,]), col='black')
+  hist(Samples$X[i,], probability=T, breaks=20, col='red', main=paste("Hist of X[",i,"]"))
+  if(exists("Baseline.Samples")) {
+    lines(density(Baseline.Samples$X[i,]), col='black')
+  }
 }
 
+message()
+Samples #return for future use, in case you care
+}  
 
-print(runtime1)
-print(runtime2)
+
+message()
+message("Starting test runs")
+message("------------------")
+Baseline.Samples = test(rNIW.extremelynaive)
+test(rNIW.naive)
+test(rNIW.snappy1)
+#test(rNIW.snappy2)
