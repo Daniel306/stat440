6b85172c5f8ffb86eef4902088072a86d2abb740
kousu <nick@kousu.ca>
2014-04-25 06:22:10 -0400

    That was a durp if I ever saw a durp
    Yeah my commit messages are getting bad. DEAL WITH IT. :P

--- R/test.correctness.R
+++ R/test.correctness.R
@@ -287,13 +287,14 @@ plot.convergence <- function(ground, samples, statistic, accumulator, title=NULL
                                     # XXX TODO: if we're only going to print 75 points anyway, maybe it isn't unreasonable to use the quadratic algorithm *at each of those specific points*
                                     # and then we can make the API nicer and kill plot.*.convergence()
     plot(samples, xlab="samples taken (n)", ylab=ylab, main=marginal.title(title, idx))        
-    #
+    
     if(!is.null(ground)) { # special case: ground can be NULL!
       ground = ..getitem..(ground, idx)
-      if(length(statistic) > 1) { #special case: DON'T stat the ground truth if there's only one of it:
+      if(length(ground) > 1) { #special case: DON'T stat the ground truth if there's only one of it:
                                    #instead, assume it is the desired value of that statistic
         ground = statistic(ground)
       }
+      stopifnot(length(ground) == 1) # at this point, ground MUST be scalar
       abline(h=ground, lty="solid", col="blue")
       legend("topleft", paste("True", statistic.name, "=", round(ground, 2)), lty="solid", col="blue")
     }
